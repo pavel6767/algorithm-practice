2202c2a380490e5252572442ba2d2df66e7e1fa0
pavel6767 <pavelmachuca@hotmail.com>
2019-09-17 00:52:53 -0400

    d13 30doc

--- 30daysOfCode/12inheritance.js
+++ 30daysOfCode/12inheritance.js
@@ -1,3 +1,17 @@
+class Person {
+  constructor(firstName, lastName, id) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.id = id;
+    // this.testScores = testScores.split(' ').map((n) => Number(n));
+  }
+
+  printPerson() {
+    console.log(`Name: ${this.lastName}, ${this.firstName}`);
+    console.log(`ID: ${this.id}`);
+  }
+}
+
 class Student extends Person {
   /*
    *   Class Constructor
@@ -8,33 +22,39 @@ class Student extends Person {
    *   @param scores - An array of integers denoting the Person's test scores.
    */
   // Write your constructor here
-
+  constructor(firstName, lastName, idNumber, testScores) {
+    super(firstName, lastName, idNumber);
+    this.testScores = testScores;
+  }
   /*
    *   Method Name: calculate
    *   @return A character denoting the grade.
    */
   // Write your method here
-  constructor(firstName, lastName, id, testScores) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.id = id;
-    this.testScores = testScores;
-  }
-
-  printPerson() {
-    console.log(`Name: ${this.lastName}, ${this.firstName}`);
-    console.log(`ID: ${this.id}`);
-  }
 
   calculate() {
-    let scores = this.testScores.split(' ');
-    let total = this.scores.reduce((prev, curr) => {
+    let total = this.testScores.reduce((prev, curr) => {
       return prev + curr;
     }, 0);
-    let avg = total / this.scores.length;
-
-    let key;
-
+    let avg = total / this.testScores.length;
     let result;
+    if (avg >= 90) {
+      result = 'O';
+    } else if (avg >= 80) {
+      result = 'E';
+    } else if (avg >= 70) {
+      result = 'A';
+    } else if (avg >= 55) {
+      result = 'P';
+    } else if (avg >= 40) {
+      result = 'D';
+    } else if (avg < 40) {
+      result = 'T';
+    }
+    return result;
   }
 }
+
+let s = new Student('firstName', 'lastName', 007, [100, 80]);
+s.printPerson();
+s.calculate();
