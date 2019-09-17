946468d6ef543d189b98bcd7f0e7d2e1ab380822
pavel6767 <pavelmachuca@hotmail.com>
2019-09-17 01:54:28 -0400

    +1

--- 30daysOfCode/12inheritance.js
+++ 30daysOfCode/12inheritance.js
@@ -3,7 +3,6 @@ class Person {
     this.firstName = firstName;
     this.lastName = lastName;
     this.id = id;
-    // this.testScores = testScores.split(' ').map((n) => Number(n));
   }
 
   printPerson() {
@@ -13,24 +12,10 @@ class Person {
 }
 
 class Student extends Person {
-  /*
-   *   Class Constructor
-   *
-   *   @param firstName - A string denoting the Person's first name.
-   *   @param lastName - A string denoting the Person's last name.
-   *   @param id - An integer denoting the Person's ID number.
-   *   @param scores - An array of integers denoting the Person's test scores.
-   */
-  // Write your constructor here
   constructor(firstName, lastName, idNumber, testScores) {
     super(firstName, lastName, idNumber);
     this.testScores = testScores;
   }
-  /*
-   *   Method Name: calculate
-   *   @return A character denoting the grade.
-   */
-  // Write your method here
 
   calculate() {
     let total = this.testScores.reduce((prev, curr) => {
--- Interview Prep Kit/Warm up/04repeatedString.js
+++ Interview Prep Kit/Warm up/04repeatedString.js
@@ -1,42 +1,7 @@
-
-// naive approach
-
-// function repeatedString(s, n) {
-
-//   if(s.length === 1) {
-//     if(s[0] === 'a') {
-//       return n;
-//     } else {
-//       return 0;
-//     }
-//   }
-
-//   let inx = 0;
-//   let newStr = 0;
-//   let count = 0;
-
-//   while(newStr<(n+s.length)) {
-//     newStr++;
-
-//     if(s[inx] === 'a') {
-//       count++;
-//     }
-
-//     inx++;
-//     if(inx === s.length-1) {
-//       inx=0;
-//     }
-//   }
-//   return count;
-// }
-
-
-// opt 2
 function repeatedString(s, n) {
-  
   // worst/best case
-  if(s.length === 1) {
-    if(s[0] === 'a') {
+  if (s.length === 1) {
+    if (s[0] === 'a') {
       return n;
     } else {
       return 0;
@@ -46,43 +11,41 @@ function repeatedString(s, n) {
   // count a's in s
 
   let oneRoundA = 0;
-  for(let i = 0; i<s.length; i++) {
-    if(s[i] === 'a') {
-      oneRoundA++
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === 'a') {
+      oneRoundA++;
     }
   }
 
   // count how many whole loops
-  let len = s.length
-  let wholeLoop = Math.floor(n/len);
-  let leftOver = n%len;
+  let len = s.length;
+  let wholeLoop = Math.floor(n / len);
+  let leftOver = n % len;
 
   let result = oneRoundA * wholeLoop;
-
-  
 
   let inx = 0;
   let newStr = 0;
   let count = 0;
 
-  while(newStr<(leftOver)) {
+  while (newStr < leftOver) {
     newStr++;
 
-    if(s[inx] === 'a') {
+    if (s[inx] === 'a') {
       count++;
     }
 
     inx++;
-    if(inx === s.length-1) {
-      inx=0;
+    if (inx === s.length - 1) {
+      inx = 0;
     }
   }
 
-  result += count
+  result += count;
   return result;
 }
 
-let str = "aba";
+let str = 'aba';
 let num = 10;
 // 7
 
