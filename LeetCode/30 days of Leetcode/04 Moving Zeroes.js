a108ba782e0619551035a71416557bed1c73275b
pavel6767 <pavelmachuca@hotmail.com>
2020-04-04 11:40:46 -0400

    30 day leetcode challenge

---
+++ LeetCode/30 days of Leetcode/04 Moving Zeroes.js
@@ -0,0 +1,65 @@
+/*
+
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+/* O(n^2) */
+// var moveZeroes = function (nums) {
+//   for (let i = nums.length - 1; i >= 0; i--) {
+//     if (nums[i] === 0) {
+//       nums.push(0);
+//       nums.splice(i, 1);
+//     }
+//   }
+//   return nums;
+// };
+
+/* O(n) */
+var moveZeroes = function (nums) {
+  let j = 0;
+  let temp;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
+
+      j++;
+    }
+  }
+  return nums;
+};
+
+let cases = [
+  { in: [0, 1, 0, 3, 12], out: [1, 3, 12, 0, 0] },
+  { in: [0, 0, 1], out: [1, 0, 0] },
+  { in: [0, 1, 0, 3, 12], out: [1, 3, 12, 0, 0] },
+];
+
+function tester(cases, cb) {
+  let current;
+  for (let i = 0; i < cases.length; i++) {
+    current = cb(cases[i].in);
+    console.log(`\n${cases[i].in} ::: ${cases[i].out}`);
+    console.log(
+      JSON.stringify(cases[i].out) === JSON.stringify(current)
+        ? 'pass'
+        : `fail :: ${current}`,
+    );
+  }
+}
+tester(cases, moveZeroes);
---
+++ LeetCode/easy/283. Moving Zeroes.js
@@ -0,0 +1,63 @@
+/*
+
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+var moveZeroes = function (nums) {
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] === 0) {
+      nums.push(0);
+      nums.splice(i, 1);
+    }
+  }
+  return nums;
+};
+
+/*  NOT modifying array */
+// var moveZeroes = function (nums) {
+//   let res = [];
+//   let count = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === 0) {
+//       count++;
+//     } else {
+//       res.push(nums[i]);
+//     }
+//   }
+//   res.push(...Array(count).fill(0));
+//   return res;
+// };
+
+let cases = [
+  { in: [0, 1, 0, 3, 12], out: [1, 3, 12, 0, 0] },
+  { in: [0, 0, 1], out: [1, 0, 0] },
+  { in: [0, 1, 0, 3, 12], out: [1, 3, 12, 0, 0] },
+];
+
+function tester(cases, cb) {
+  let current;
+  for (let i = 0; i < cases.length; i++) {
+    current = cb(cases[i].in);
+    console.log(`\n${cases[i].in} ::: ${cases[i].out}`);
+    console.log(
+      JSON.stringify(cases[i].out) === JSON.stringify(current)
+        ? 'pass'
+        : `fail :: ${current}`,
+    );
+  }
+}
+tester(cases, moveZeroes);
