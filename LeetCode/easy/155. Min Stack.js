90593c13caae334e73b0cd82e5fed8fc15129bbc
pavel6767 <pavelmachuca12@gmail.com>
2020-06-19 11:30:15 -0400

    Update 155. Min Stack.js
    added more time efficient

--- LeetCode/easy/155. Min Stack.js
+++ LeetCode/easy/155. Min Stack.js
@@ -25,46 +25,95 @@ Example:
  * initialize your data structure here.
  */
 
-function StackNode(val) {
-  this.val = val;
-  this.min = null;
-}
+// function StackNode(val) {
+//   this.val = val;
+//   this.min = null;
+// }
 
-var MinStack = function () {
-  this.arr = [];
-};
+// var MinStack = function () {
+//   this.arr = [];
+// };
+
+// /**
+//  * @param {number} x
+//  * @return {void}
+//  */
+// MinStack.prototype.push = function (x) {
+//   let newNode = new StackNode(x);
+//   newNode.min = this.getMin() < x ? this.getMin() : x;
+//   this.arr.push(newNode);
+// };
+
+// /**
+//  * @return {void}
+//  */
+// MinStack.prototype.pop = function () {
+//   this.arr.pop();
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MinStack.prototype.top = function () {
+//   return this.arr[this.arr.length - 1].val;
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MinStack.prototype.getMin = function () {
+//   return this.arr.length === 0
+//     ? Number.POSITIVE_INFINITY
+//     : this.arr[this.arr.length - 1].min;
+// };
+
+
+/*
+sol 2
+*/
 
 /**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    this.stack = []
+    this.min = []
+};
+
+/** 
  * @param {number} x
  * @return {void}
  */
-MinStack.prototype.push = function (x) {
-  let newNode = new StackNode(x);
-  newNode.min = this.getMin() < x ? this.getMin() : x;
-  this.arr.push(newNode);
+MinStack.prototype.push = function(x) {
+    const {stack, min} = this
+    stack.push(x)
+    if(min.length === 0 || min[min.length-1] >= x) {
+        min.push(x)
+    }
 };
 
 /**
  * @return {void}
  */
-MinStack.prototype.pop = function () {
-  this.arr.pop();
+MinStack.prototype.pop = function() {
+    const pop = this.stack.pop()
+    if(pop === this.min[this.min.length-1]) {
+        this.min.pop()
+    }
 };
 
 /**
  * @return {number}
  */
-MinStack.prototype.top = function () {
-  return this.arr[this.arr.length - 1].val;
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length-1]
 };
 
 /**
  * @return {number}
  */
-MinStack.prototype.getMin = function () {
-  return this.arr.length === 0
-    ? Number.POSITIVE_INFINITY
-    : this.arr[this.arr.length - 1].min;
+MinStack.prototype.getMin = function() {
+    return this.min[this.min.length-1]
 };
 
 function tester() {
